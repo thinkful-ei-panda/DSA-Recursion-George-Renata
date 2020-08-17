909d95657b2f4438e21ccef248717f0bbfb2da58
seraphyne <renatafd717@gmail.com>
2020-08-17 12:43:32 -0500

    fixed a typo

--- recursion.js
+++ recursion.js
@@ -11,7 +11,7 @@ console.log(countingSheep(8));
 
 // 2- Power Calculator - Write a function called powerCalculator() that takes two parameters, an integer as a base, and another integer as an exponent. The function returns the value of the base raised to the power of the exponent. Use only exponents greater than or equal to 0 (positive numbers)
 
-const powerCalculator() = function(base, exponent){
+const powerCalculator = function(base, exponent){
     if(exponent === 1){
         return base;
     }
