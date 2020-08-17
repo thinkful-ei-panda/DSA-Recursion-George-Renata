aa0cac04599050814e9da58893d39bf5ad4d0fcf
seraphyne <renatafd717@gmail.com>
2020-08-17 12:54:08 -0500

    added 12

--- recursion.js
+++ recursion.js
@@ -84,3 +84,13 @@ console.log(factorial(8));
 
 
 // 12 - Binary Representation - Write a recursive function that prints out the binary representation of a given number. For example, the program should take 3 as an input and print 11 as output, or 25 as an input and print 11001 as an output. Note that the binary representation of 0 should be 0.
+
+function convertBinary(input){
+  if(input >0){
+    let binaryForm = Math.floor(input % 2);
+    return (convertBinary(Math.floor(input / 2)) + binaryForm);
+  }
+  else{
+    return '';
+  }
+}
